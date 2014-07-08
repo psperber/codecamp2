e2e1c8d50821a7c449b810c07f8489e35784bb90
sperb <Comtec@141.51.114.80>
2014-07-08 16:42:11 +0200

    ...

---
+++ Challenger/src/de/uks/challenger/ui/setup/Setup2Fragment.java
@@ -0,0 +1,37 @@
+package de.uks.challenger.ui.setup;
+
+import de.uks.challenger.R;
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Setup2Fragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_setup2, container,
+				false);
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+	}
+
+	/**
+	 * Returns a new instance of this fragment for the given section number.
+	 */
+	public static Fragment newInstance() {
+		Setup2Fragment fragment = new Setup2Fragment();
+		Bundle args = new Bundle();
+		// args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
+}
